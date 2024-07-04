9274cde166ff457bb8e51a4ddf5a91e709d5f311
francis2911 <138940663+francis2911@users.noreply.github.com>
2024-07-04 16:53:25 -0400

    prueba3

---
+++ MainPage.jsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from 'react';
+import PageTitle from 'components/PageTitle';
+import { PageContainer, FormContainer } from './MainPage.styled';
+
+import Container from 'components/Container';
+import { Modal } from 'components/Modal';
+import CalculatorСalorieForm from 'components/Forms/CalculatorСalorieForm/CalculatorСalorieForm';
+import { useMobileModal } from 'hooks/ui';
+import { useTranslation } from 'react-i18next';
+
+const MainPage = () => {
+  const [showModal, setShowModal] = useState(false);
+  const openModal = () => setShowModal(prev => !prev);
+
+  const [showMobileModal, , hideMobileModal] = useMobileModal();
+
+  useEffect(() => {
+    return () => {
+      if (showModal || showMobileModal) {
+        hideMobileModal();
+        setShowModal(false);
+      }
+    };
+  }, [hideMobileModal, showMobileModal, showModal]);
+
+  const { t } = useTranslation();
+  return (
+    <Container>
+      <PageContainer>
+        <PageTitle title={t('title')} />
+        <FormContainer>
+          <CalculatorСalorieForm openModal={openModal} />
+        </FormContainer>
+
+        {(showModal || showMobileModal) && (
+          <Modal showModal={showModal} setShowModal={setShowModal} />
+        )}
+      </PageContainer>
+    </Container>
+  );
+};
+
+export default MainPage;
